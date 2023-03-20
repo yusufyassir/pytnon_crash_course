d4a7e3547b9c4d7cdae053f8b39b9db0e461aced
yusufyassir <yusufyassir0@gmail.com>
2023-03-20 08:25:09 +0300

    1

--- chapter8/favorite_book.py
+++ chapter8/favorite_book.py
@@ -2,5 +2,4 @@ def favorite_book(title):
     """this function print favorite book"""
     print(f"one of my favorite books is {title.title()}.")
 
-favorite_book(''
-              ' to great')
+favorite_book('good to great')
